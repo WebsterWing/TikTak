7c1774bb7e487db8f22ca3829dd656167eb0dd92
Webster Wing <webster.wing@gmail.com>
2020-12-09 16:23:35 -0600

    test running time

--- TikTakAPI/src/main/java/com/cognizant/TikTakApiApplication.java
+++ TikTakAPI/src/main/java/com/cognizant/TikTakApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+// do github actions run faster after the first time?
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class}) // allow startup without data source
 public class TikTakApiApplication {
