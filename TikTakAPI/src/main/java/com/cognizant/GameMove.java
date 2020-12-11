06c6ed37d52bbc02a8407a797cb31ae78f49aa79
Isaac Lo <isaaclo123@gmail.com>
2020-12-11 13:05:09 -0800

    dynamodb start

---
+++ TikTakAPI/src/main/java/com/cognizant/GameMove.java
@@ -0,0 +1,52 @@
+package com.cognizant.TikTak;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
+@DynamoDBDocument
+public class GameMove {
+	private int row;
+	private int col;
+	private String player;
+	private String username;
+	public GameMove(int row, int col, String player, String username) {
+		super();
+		this.row = row;
+		this.col = col;
+		this.player = player;
+		this.username = username;
+	}
+	public GameMove() {};
+
+	@DynamoDBAttribute(attributeName = "row")
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	@DynamoDBAttribute(attributeName = "col")
+	public int getCol() {
+		return col;
+	}
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	@DynamoDBAttribute(attributeName = "player")
+	public String getPlayer() {
+		return player;
+	}
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+
+	@DynamoDBAttribute(attributeName = "username")
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
